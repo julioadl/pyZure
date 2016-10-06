56a6cc4318b14567cf625bdcb9a5cfff60bff0de
Julio Amador <julioadl@gmail.com>
2016-10-06 13:51:00 +0100

    Added wrapper for sentiment analysis

--- pyZure.py
+++ pyZure.py
@@ -2,6 +2,7 @@
 import requests
 import json
 
+#Bing News
 #Key (required)  is a string needed to access bing resources
 #query (required) is a string indicating the search query. If more than one word, the queries should be concatenated with '&'
 #count (optional) is an integer ranging bewteen 10 and 100 indicating the number of results bing will return
@@ -15,6 +16,22 @@ def bingNews(key, query, count = 10, market = 'en-us'):
 
 #Send request
     r = requests.get(url = bingNews_url, headers = header)
+    data = json.loads(r.text)
+
+    return data
+
+#Sentiment api
+#Key (required)  is a string needed to access bing resources
+#data (required) is a JSON formatted dictionary containing the documents to be analyzed - to see the exact format see https://azure.microsoft.com/en-us/documentation/articles/cognitive-services-text-analytics-quick-start/
+#To get more info visit https://azure.microsoft.com/en-us/documentation/articles/cognitive-services-text-analytics-quick-start/
+def sentimentAPI(key, data):
+
+    #Set requeste
+    sentimentAPI_url = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment/'
+    header = {'Ocp-Apim-Subscription-Key': key}
+
+    #Send requests
+    r = requests.post(url = sentimentAPI_url, headers = header, data = data)
     data = json.loads(r.text)
 
     return data
